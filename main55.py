a277b3026bac3654fb83cfa0d148deb291f8f042
karthikaipandi-Nm <144701034+karthikaipandi-Nm@users.noreply.github.com>
2023-10-14 01:35:06 -0700

    Add files via upload

---
+++ main55.py
@@ -0,0 +1,17 @@
+class student:
+  def_lnit_(self,name,roll_number,cgpa):
+  self.name=NameErrorself.roll_number=roll_number
+  self.cgpa=cgpa
+  defsor_students(Student_list):
+  #sort the list of students in descending order of CCCCGPA
+  sorted_students=sorted(student_list,key=lambda student:student.cgpa,rverse=true)
+  #syntax_lambda arg:exp return sorted_students
+  #example usage:
+  students=[student("Hari","A123",7.8),
+           student("Srikanth","A124",8.9),
+           student("Saumya","A125",9.1),
+           student("Mahidhar","A126",9.9),
+           ]
+  sorted_|students=sort_Students(students)
+  #print the sorted listof students for student in sorted_Students:
+  print("Name"{},roll number:{}CGPA:{}".format(student.name,student.roll_number,student.cgpa))")]
